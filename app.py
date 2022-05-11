368ab3b63a2049268733af7a211ef229c7d8a6ca
Cheddar <ivn.rojas@outlook.com>
2022-05-11 15:07:52 -0300

    🌟 - First stable version. Test file deleted.

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 import requests
+from datetime import datetime
+import time
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+current_appointment = datetime.strptime('2022-09-15', '%Y-%m-%d')
 
 cookies = {
     '_ga': 'GA1.2.1697804321.1652268137',
     '_gid': 'GA1.2.1671280518.1652268137',
-    '_yatri_session': 'V0Y4SjlaMUZCb2U1K0dqR3RtYkhsN3NmTVNkZHNsVkRFcFZxRzJDeTZ0QUE2UnVXQTVBVTNjQmsvUFNRcFJZNU5KcURBWjRQR2NkZFhDcXpRRnMxODRreGhRbmhlU0JmbzZpNytHdy9yUy9yS0U5R1hkTW5YOXJyaVZMUXFINHNHbkRNSFRZUG5ncXIvOGNra0pVQ1NLSmVselVhUzhiWXVSYjVJSE1xZmZ4eTF1eUZrTThwQkRvV0ZLcWpFSFhNcTNOV3FOcUxNZ2lmVkNpY3dDY0NsWU02Y1JGeEZtYzByR2x5RjEwYjh3R0tISFhnR1lxbC83NUUxWVptTjZNQlB6ZXVaWTN4bGF6a3ViY05jaDFXUjRMKzdCQWZtMzVpL1doVmNaU2xlMHhYZ0IwVVBiUUc2Y01iSVR5Y0ZWckFJb0pPVnJxc20yQld6bUltY0tUUVhzdUMvSUtoSXpvbWhmWklFYjNBa1Z5UklZVDl3bHpkWVdVVVZWTERwM1FCYVRnTXhnMXEvMGJZMjBCTWVhbE9aQzF6M2xKRzZHWWtZc3Y2ckFwUDhQQm12Z3NKSGtWdmlEeHI1SHkyZXl3azgzTlM2RGZ5STRadlBRczhTeGVNVHg2T0lYV0ZTTGYxRWFpU3FkWVprdjN0RjlvTzJJMUF3Q0dZRGM3RU0zbmMxZ2x0dEpGK28yVjdLU1hFRlJ6SFNPUSs1VlNUNmFZQjd5OTB6RjNsd3ZvPS0tdHd5L1RjejBCd29sa0w3WXMrN3ZlUT09--119af872fb3da5cd8a542727e4ea4b94d4ce85dd',
+    '_yatri_session': 'K0U2VHhsZVNhSitnN3Rmd1lWWUpCSDZhMVZaRSthVHBma0E5cUVCa1VhRFhxOHhPR29UWWs0TnpCdVpPd3FvOFRBZSs5SHg2YWF2RkR4V2pmdWk5dXQyL1luTWxVRC82cVhNekFBVzZrcnB2cmszYXYvU1BXb2RZMjhnaEwvYkJiaTJabTJHUFNGdllMbWdsd21HYTdENzdFSHkzWHlKWjcyZmJRMG5OMzk2ZlVkSjV2dEJDa0hQdFpwbmNnYnB4NjdNelhTSGNFalhudjFMRGRiaDdLdjJtdGh5b2QzVzRxaTUxN3JwTzRmdTljVUhNRDlDc1JubExZZlE1dEpaMExMMnBKNURUK2R1eWRxSlpmRSs4L0hOck1BSDh1SGZyYVdDc3FndU9HNVN1UUVVM1ZiT1MrNVBYT1gxL2VOUXU5MHhEZ09ROGFKTC9BL1JMNlNFTjNWbHV0UTMxd1VreUZTWWozSUV2cUY1N215SnhuK1lMM2tnMDV6N2JabFMwNDFwWG9kQTBqb2U2MHoyeTQ1anBQQ0VnNjhoSEhxVm15T0lQUEtoVEUwWFVyTXFBK29kcXN4MzVZaDRmUy8vYnZhaXlETFRwbHhzQ05ENHMyM3oxTm5uZzdiUytpR1Jkd3VlN0dUNDZnRWVFQml2Ty8rTkNnOE00QW9pVnFnSTU5Z21DY3Z1VlBlREg2ZU9pK2g2OFdNK3ZxV2UvUDlXdThBdE5wOXhENEl3PS0tamk5T2JxNTJscElDU3hWWXBxbjhlZz09--813a2aa8e427067ce4cd49b0e861cd3f841469a0',
 }
 
 headers = {
@@ -20,9 +35,32 @@ headers = {
     'Sec-Fetch-Dest': 'empty',
     'Referer': 'https://ais.usvisa-info.com/es-ar/niv/schedule/38923059/appointment',
     'Accept-Language': 'en-US,en;q=0.9,es-AR;q=0.8,es;q=0.7',
-    # Requests sorts cookies= alphabetically
-    # 'Cookie': '_ga=GA1.2.1697804321.1652268137; _gid=GA1.2.1671280518.1652268137; _yatri_session=VmtZZE01VUtieDBDUXJRN2lBbHdLbW5XdllhdytlMmVDbUNFRUhmVC9KL1NZclpCelR1Vkl0N2tlN2lNa0RaMDhFbUxqWXZWMXRlQ1FCVHd5WkVJaU1yZEVIVXZ1RGZqbndaZlZKUUxjMmV1eE5rWjdvNjFUeHY4Smo1aUtDQ254RnJGNHN4WGY2cURCenM2SkdSQXFoNVZ5TXhDaytWaDdxWlZRUnRuVnJxcXpldGM5dU9pQUdKV0ZGWjF2aTRsODlRU1RjNU9sLzlsM1Y2Y2xkcUFYUzdlSGtvOWYxRVR2VURURzNERG50RHZjWWs0QTMwSjZndmFVNTZlWXBuYW4yM1FqcDh3VHNGa3pQZGpqM0hsUnVvY2h0amxqYTdqZGU4OG5wNm1NNE9SMGV2eDU0NU51MHIrSTg2QXloMTZVTXExR1MreHlBWkhsVmRtTlpVSGVCdzM0MGJkbFlXSFVFaWNicHAveVJaYnFRV0NKVWlaTDFZYUtPZWo3N01KNHdpYVpkeldHVUxQS2VUd1ZOQ3JOSnNFa1BReHV3aDV4ZGFvRVlGNnhpdHJwd2krNFg4VWxicDdKb2g1T1Z2S0ZQaHAxbGFCSU5SREFxNDcyRjlzdDZwR2x2b01udUxGMVB6KzlWSC9EMHpoR1VRY29RYjVrMldsRit0S2dCbnpWZmwrb2VVeHhNRDJEeldwcE5NbTF4ZVZQUlVRa3dYUTNvM3NxVFRWVlVVPS0tZ0hmUnpvaHJ3SmJnVXZvYlJzUllWZz09--be6a049bf2b7e8854835ce99b6e563f2cde45709',
 }
 
-response = requests.get('https://ais.usvisa-info.com/es-ar/niv/schedule/38923059/appointment/days/28.json?appointments\\[expedite\\]=false', cookies=cookies, headers=headers)
-print(response.json())
+# response = requests.get('https://ais.usvisa-info.com/es-ar/niv/schedule/38923059/appointment/days/28.json?appointments\\[expedite\\]=false', cookies=cookies, headers=headers)
+# print(response.json())
+
+found = 'NOTHING FOUND'
+
+while True:
+    now = datetime.now().strftime('%H:%M:%S %d/%m')
+    response = requests.get('https://ais.usvisa-info.com/es-ar/niv/schedule/38923059/appointment/days/28.json?appointments\\[expedite\\]=false', cookies=cookies, headers=headers)
+    appointments = response.json()
+
+    top_five_appointments = appointments[:5]
+    
+    print(now+' | '+found+'\n-------------------------')
+
+    for item in top_five_appointments:
+        # Transform the date string into a datetime object
+        datetime_obj = datetime.strptime(item['date'], '%Y-%m-%d')
+        # Get a well formatted string of the datetime object
+        datetime_str = datetime_obj.strftime('%d-%b-%Y')    
+        if current_appointment > datetime_obj:
+            print(f'{bcolors.BOLD}{bcolors.OKCYAN}{datetime_str}{bcolors.ENDC}')
+            found = f'{bcolors.BOLD}{bcolors.FAIL}LAST FOUND AT {now}{bcolors.ENDC}'
+        else:
+            print(f'{bcolors.ENDC}{datetime_str}')
+    
+    print('-------------------------\n')
+    time.sleep(10)
