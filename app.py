d75cbe7ebbf7c3c1758109ba344a8cec71049f56
Ivan <ivn.rojas@outlook.com>
2023-06-15 19:58:08 -0300

    🆔 - Readme update

--- app.py
+++ app.py
@@ -15,30 +15,31 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 current_appointment = datetime.strptime('2023-12-01', '%Y-%m-%d')
+ugly_appointment = datetime.strptime('2023-06-21', '%Y-%m-%d')
 
 cookies = {
-    '_ga': 'GA1.2.1136866613.1686784309',
-    '_gid': 'GA1.2.1139931980.1686784309',
+    '_ga': 'GA1.2.854241653.1686842990',
+    '_gid': 'GA1.2.586893050.1686842990;',
     '_gat': '1',
-    '_yatri_session': 'ZDBTNnF2dnJqcTVNRVJqM3NjZnNRb0NjNm1QcHZOTU5heExESWdZTVUrMDJDWmlXckV1aVk1MnZlRGpSUTJOQkdkQXlFRFJVOHQ2bFlnT25Wa0tFZFhEczlsd1Y3Y21aR1Rlcm5ONEFVZGRBdm0wZFZId3IyUVZnR241WUtUeDhld3BuNytkeTN0bFFDYXBMRVZIK0s5WWwwU250UEJpdjVHYU1wczRYN1RVZG04Q3Z6N0loOUFrbWRCaUgxcEJ4RmZBblVSUzZYNm5MQUlKeDZHYVVqU2NLVXlUdlFpQ2k5cTdSYy9LTDhtaFNTUVlOa1JBUnpabTNJbmZ5WU5vTTZHWDNNZEhid25DcDl5Mk83TG1zell4Z1VGNy90VWZicnpDVmJYZjF2L0Y4T21DM2VQWk9EaE1SdkVpdmh0djQzVWlvZEJUR0RoblIvbHQ0RjBCL0YwQnBQaC95cktwc01hd3BzN3NVQnNOVFpKVS8zTUF3KzBqT0hneFpaSFJYMFppS3htdDh3NmJERytoUXJUNUNyNVVtemFSTlFRWHBvOVZFb0wyOWp1aVdmNmpZUXQ2ZXVBTWRmZjZ1S0pTK1FSN1dyN0lhN2FVLy9paFVXdTRCS2dJWU5lcXpJNjlwQStSTDJKTWR4bUNQbVhIekorRWRsY0JFTmUzRlc5RGtsQzhLcjJ0RlJKYVVBUThhR1d6K0pwNHVPOGZObDBieW11OUNWMWFYdC9jPS0tbWdHd1NuNzNORHk0UGY1QktHajJwQT09--3f6c7e8c00b0753df8a76004ad4c6e8b583723a7',
+    '_yatri_session': 'UjN1dDltVDRLcG15aHpUL0ZqRk9JaHRVYzFmS2IvNzlRb25XTHFVV2tlL0ZSeGoxeW9pNlNPVTNweGRtWGhZOFA1eEpRRDFDNkhNN1lQaVdGTktBSFVwSmFIUk9UOW9WOU03aWZNdnpVK1hGTjlPUXh2aU81aGVPS0U2cWxMamdHOHRFM3hYYUM4NWdLeDFOM2hlQjBBaVRPNXFORHppcDlaOW5mRXVuWUUxeTJ3RzFJcnlwQW5wRlh2bFVGVlhoNDVBaXZJdlRvQStySlQ3NEs3czBIZWwweEs5K1FxZkhwVG5GQ0xKYUdYZ0NkdUZGeWdlMm12VkswYTNoTU5nNkRSRnF5eHJTTGdwb3M3ZDdxMGJtMDlxRGdMb05kY2ZJZ3NHZUFqN1JDVERHcHgrQXp4VWdNVFVGOXEvN2xwZ1ZQNnozbjNPSXdUOVlocFNOVUxvNmhyQk0yZUFvNjQ4NFQ0eFhsSktZMm1Ta0ZLTlJOYWxmOGV4c1J4aGY3cUIvTVU0NXg5MG9PUU9wVXYyUUhZaTVHeFdxMGcyN0lBclVTV1ZoZ1FVdmNDQkYyK3A5aE9xSDNZMTRVZE5paWI1ZXh2allOQkp2K0dUVXp5V1E5OWxxNElDNlhUMi9TTDIxRlZ2R2o5RFJhd2JHSVhYZG1CKzdOeWRtYTl4UGpEV1E1TUFldnQ1b1Nqb2E0MXQxbFJ1V1JsaThjVS9VaDh4NU1GcWRtMW5YSDBZPS0tNmM5aXV1U0tpSUFOQ1JyMExVTUdYZz09--1bddb4f27a83dad80159411d21356c957161cbb0',
 }
 
 headers = {
     'Accept': 'application/json, text/javascript, */*; q=0.01',
     'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9,es-AR;q=0.8,es;q=0.7',
+    'Accept-Language': 'es',
     'Connection': 'keep-alive',
     'Host': 'ais.usvisa-info.com',
-    'If-None-Match': 'W/"8376c608a41bf791cfc2c2bd5faf21ca"',
-    'Referer': 'https://ais.usvisa-info.com/es-ar/niv/schedule/38923059/appointment',
-    'sec-ch-ua': '"Google Chrome";v="108", "Chromium";v="108", ";Not A Brand";v="8"',
+    'If-None-Match': 'W/"01e609dfbd6f5b9fe2541a1615852fb7"',
+    'Referer': 'https://ais.usvisa-info.com/es-ar/niv/schedule/49613228/appointment',
+    'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
     'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Linux"',
+    'sec-ch-ua-platform': '"macOS"',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-    'X-CSRF-Token': 'XphOK5t5jys8mh8/XBfsmcDpljsrR4YdJVTQc94w4p5eJcdahzufdpJq3XkYD3o9OsURF+wjldcN1geIcj6GMw==',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    'X-CSRF-Token': 'qCAKG2y7+/iXzxyqdiXOKiwRzyzn6j0DrfSi5jEeScJ4vz/BMsUWPrIKDwQiWRT36HfrExRURrS3Mvy37WhLbg==',
     'X-Requested-With': 'XMLHttpRequest',
 }
 
@@ -47,12 +48,12 @@ found = 'NOTHING FOUND'
 while True:
     now = datetime.now().strftime('%H:%M:%S %d/%m')
     response = requests.get('https://ais.usvisa-info.com/es-ar/niv/schedule/49613228/appointment/days/28.json?appointments\\[expedite\\]=false', cookies=cookies, headers=headers)
-    appointments = response.json()
-
     print('-text')
     print(response.text)
     print('-header')
     print(response.headers)
+
+    appointments = response.json()
 
     top_five_appointments = appointments[:5]
     
@@ -63,7 +64,7 @@ while True:
         datetime_obj = datetime.strptime(item['date'], '%Y-%m-%d')
         # Get a well formatted string of the datetime object
         datetime_str = datetime_obj.strftime('%d-%b-%Y')    
-        if current_appointment > datetime_obj:
+        if current_appointment > datetime_obj and datetime_obj != ugly_appointment:
             print(f'{bcolors.BOLD}{bcolors.OKCYAN}{datetime_str}{bcolors.ENDC}')
             found = f'{bcolors.BOLD}{bcolors.FAIL}LAST FOUND AT {now}{bcolors.ENDC}'
             notification_url = 'https://cheddar-api.onrender.com/notification'  
